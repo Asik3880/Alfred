d30f5633c9fc23e5f64c18f0b12d4fbc0fc63144
Asik3880 <141816024+Asik3880@users.noreply.github.com>
2024-05-25 17:19:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Asik3880/Alfred.git /Fm-Filter-Bot
+  git clone https://github.com/Asik3880/Alfred /Fm-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Fm-Filter-Bot
